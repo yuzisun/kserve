10f7452c97cc05885c6bf2db8565d079e1b36796
Gavin Li <yli1109@bloomberg.net>
2024-12-13 20:29:06 +0800

    LocalModelCache Admission Webhook (#4102)
    * init
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    * broken code
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    * register webhook
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    * rename + working
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    * pass in client
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    * check storageURI
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>
    
    ---------
    
    Signed-off-by: Gavin Li <yli1109@bloomberg.net>

--- pkg/controller/v1beta1/inferenceservice/rawkube_controller_test.go
+++ pkg/controller/v1beta1/inferenceservice/rawkube_controller_test.go
@@ -22,6 +22,8 @@ import (
 	"time"
 
 	"github.com/kserve/kserve/pkg/apis/serving/v1alpha1"
+	"github.com/kserve/kserve/pkg/utils"
+	apierr "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
